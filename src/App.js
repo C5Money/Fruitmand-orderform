3f462360588acc2b0ae7f58a4a809f2555e652df
C5Money <misriffi@gmail.com>
2023-01-23 12:40:23 +0100

    Eerste deel opdracht

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, {useState} from 'react';
 import './App.css';
 import bedrijfsLogo from "./assets/screenshot-logo.png";
-import aardbei from "./assets/aardbei.png";
-import banaan from "./assets/banaan.jpg";
-import appel from "./assets/appel.jpg";
-import kiwi from "./assets/kiwi.jpg";
-import Fruitblok from "./components/Fruitblok";
+import aardbeiPic from "./assets/aardbeiPic.png";
+import banaanPic from "./assets/banaanPic.jpg";
+import appelPic from "./assets/appelPic.jpg";
+import kiwiPic from "./assets/kiwiPic.jpg";
 import Button from "./components/Button";
 
 
 function App() {
 
-    const [counter, setCounter] = useState(0);
+    const [aardbei, setAardbei] = useState(0);
+    const [banaan, setBanaan] = useState(0);
+    const [appel, setAppel] = useState(0);
+    const [kiwi, setKiwi] = useState(0);
 
-    function minus() {
-        setCounter(counter -1) ;
-    }
-
-    function plus() {
-        setCounter(counter +1);
+    function resetFruits(){
+        setAardbei(0);
+        setBanaan(0);
+        setAppel(0);
+        setKiwi(0);
     }
 
   return (
@@ -28,20 +29,42 @@ function App() {
                 <img src={bedrijfsLogo} alt="Bedrijfslogo"/>
             </section>
 
-            <section className="fruitblok">
-                <img src={aardbei} alt={aardbei} className="fruitImg"/>
-                <h3>Aardbei</h3>
+            <section >
+                <article className="fruitblok">
+                    <img src={aardbeiPic} alt="aardbei" className="fruitImg"/>
+                    <h3>Aardbei</h3>
+                    <Button type={ "button" } className={ "counter-btn"} clickHandler={ () => {setAardbei(aardbei -1)} } isDisabled={ aardbei === 0 }>-</Button>
+                    <p>{aardbei}</p>
+                    <Button type={"button"} className={"counter-btn"} clickHandler={ () => {setAardbei(aardbei +1)} }>+</Button>
+                </article>
 
-                <Button type={ "button" } className={ "counter-btn"} clickHandler={ minus } disabled={counter === 0}>-</Button>
+                <article className="fruitblok">
+                    <img src={banaanPic} alt="banaan" className="fruitImg"/>
+                    <h3>Banaan</h3>
+                    <Button type={ "button" } className={ "counter-btn"} clickHandler={ () => {setBanaan(banaan -1)} } isDisabled={ banaan === 0 }>-</Button>
+                    <p>{banaan}</p>
+                    <Button type={ "button" } className={ "counter-btn"} clickHandler={ () => {setBanaan(banaan +1)} }>+</Button>
+                </article>
 
-                <p>{counter}</p>
-                <button type="button" className="counter-btn" onClick={() => {setCounter(plus)}}>+</button>
+                <article className="fruitblok">
+                    <img src={appelPic} alt="appel" className="fruitImg"/>
+                    <h3>Appel</h3>
+                    <Button type={ "button" } className={ "counter-btn"} clickHandler={ () => {setAppel(appel -1)} } isDisabled={ appel === 0 }>-</Button>
+                    <p>{appel}</p>
+                    <Button type={ "button" } className={ "counter-btn"} clickHandler={ () => {setAppel(appel +1)} }>+</Button>
+                </article>
+
+                <article className="fruitblok">
+                    <img src={kiwiPic} alt="kiwi" className="fruitImg"/>
+                    <h3>Kiwi</h3>
+                    <Button type={ "button" } className={ "counter-btn"} clickHandler={ () => {setKiwi(kiwi -1)} } isDisabled={ kiwi === 0 }>-</Button>
+                    <p>{kiwi}</p>
+                    <Button type={ "button" } className={ "counter-btn"} clickHandler={ () => {setKiwi(kiwi +1)} }>+</Button>
+                </article>
+
+                <Button type={ "button" } className={ "reset-btn" } clickHandler={ () => { resetFruits() } }>Reset</Button>
             </section>
 
-
-
-            <section></section>
-            <section></section>
         </main>
     </>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Button({ type, className, clickHandler, children}) {
+function Button({ type, className, clickHandler, isDisabled, children}) {
     return (
         <button
             type={ type }
             className={ className }
             onClick={ clickHandler }
-
+            disabled={isDisabled}
         >
             { children }
         </button>
